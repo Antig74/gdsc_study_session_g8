99959e568172d8fce583b64bb5aa46f53e90b220
Anti74 <antenehgetnet09@gmail.com>
2023-12-05 02:33:59 -0800

    adding to do list app with full UI

--- BMI.js
+++ BMI.js
@@ -5,6 +5,7 @@ function calculateBmi() {
     let height= document.getElementById('height').value
 
     let bmi= weight / (height * height)
+    Math.round(bmi, 2)
 
     document.getElementById('heading').innerHTML = 'Your BMI is: '
     document.getElementById('bmi-output').innerHTML = bmi
